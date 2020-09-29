294c76550c6e04e5efb0d2021f3e30d4895833b1
Eric Mayo <eric.mayonnaise@gmail.com>
2020-09-29 10:09:47 -0400

    Added user authentication to the script.

--- FirstScript.sh
+++ FirstScript.sh
@@ -6,4 +6,21 @@ upgrade(){
     sudo apt-get upgrade --force-yes -y
 }
 
-upgrade
+authentication(){
+    echo "Do you want to update and upgrade this system? Yes or No"
+    read ans
+    case $ans in
+        "Yes" | "yes" | "Y" | "y" )
+            upgrade
+            ;;
+        "No" | "no" | "N" | "n" )
+            exit
+            ;;
+        * )
+            echo "Please enter a valid input."
+            authentication
+            ;;
+        esac
+}
+
+authentication
